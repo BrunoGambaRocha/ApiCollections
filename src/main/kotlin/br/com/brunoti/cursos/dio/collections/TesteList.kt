1ff92f3e506af83bf72209800420c0eee1704ca2
BrunoGambaRocha <contato@brunoti.com.br>
2021-07-26 12:11:37 -0300

    update list test class

--- src/main/kotlin/br/com/brunoti/cursos/dio/collections/TesteList.kt
+++ src/main/kotlin/br/com/brunoti/cursos/dio/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package br.com.brunoti.cursos.dio.collections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 1000.0,"CLT")
+    val pedro = Funcionario("Pedro", 2000.0,"PJ")
+    val maria = Funcionario("Maria", 4000.0,"CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -12,11 +12,18 @@ fun main() {
 
     println("--find---Maria--------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("--sortedBy---salario--------")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("--groupBy---tipoContratacao--------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
