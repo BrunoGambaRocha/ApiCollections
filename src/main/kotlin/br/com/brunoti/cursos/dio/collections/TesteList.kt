efaae921b8ac368b2a59ab782c7761cc7621bc7f
BrunoGambaRocha <contato@brunoti.com.br>
2021-07-26 10:52:32 -0300

    created list test class

---
+++ src/main/kotlin/br/com/brunoti/cursos/dio/collections/TesteList.kt
@@ -0,0 +1,26 @@
+package br.com.brunoti.cursos.dio.collections
+
+fun main() {
+    val joao = Funcionario("João", 1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    println("--Funcionarios-----------")
+    funcionarios.forEach { println(it) }
+
+    println("--find---Maria--------")
+    println(funcionarios.find { it.nome == "Maria" })
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String =
+        """
+            Nome =  $nome
+            Salário = $salario
+        """.trimIndent()
+}
